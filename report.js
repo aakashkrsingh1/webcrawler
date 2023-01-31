aa6c741bac48799e8998e9eb3b1903f869683e82
Akash Kumar Singh <aakashkrsingh1@pop-os.localdomain>
2023-02-01 01:11:40 +0530

    Added Text Report Generation

--- report.js
+++ report.js
@@ -1,18 +1,25 @@
+const fs = require('fs');
+
 function printReport(pages){
+    const myConsole = new console.Console(fs.createWriteStream('./Report/Report-Generated-At-'+new Date()+'.txt'));
+    var docText=""
     console.log("===================")
     console.log("======REPORT=======")
+    docText+="+++++++++++++++++++++++++REPORT++++++++++++++++++++++++++++++++\n"
     const sortedPages=sortPages(pages)
     for(const sortedPage of sortedPages)
     {
         const url=sortedPage[0]
         const hits=sortedPage[1]
         console.log(`Found ${hits} links to page: ${url}`)
+        docText+=`\n Found ${hits} links to page: ${url} \n`
     }
 
     console.log("===================")
     console.log("===END OF REPORT===")
+    docText+="+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
     console.log("===================")
-    
+    myConsole.log(docText)
     
 }
 
